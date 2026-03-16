5e5099a7c1b1e09013b8facd40928a22282b4609
Satchmo <rohenaz@gmail.com>
2026-03-16 10:13:07 -0400

    OPL-1218: Switch BAP_SERVER to api.1sat.app
    api.sigmaidentity.com is currently down. Use the 1sat overlay
    API at https://api.1sat.app/1sat/bap as the default server.

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const BAP_BITCOM_ADDRESS = "1BAPSuaPnfGnSBM3GLV9yhxUdYe4vGbdMT";
 export const BAP_BITCOM_ADDRESS_HEX = toHex(toArray(BAP_BITCOM_ADDRESS));
 export const AIP_BITCOM_ADDRESS = "15PciHG22SNLQJXMoSUaWVi7WSqc7hCfva";
 export const AIP_BITCOM_ADDRESS_HEX = toHex(toArray(AIP_BITCOM_ADDRESS));
-export const BAP_SERVER = "https://api.sigmaidentity.com/v1";
+export const BAP_SERVER = "https://api.1sat.app/1sat/bap";
 export const MAX_INT = 2147483648 - 1; // 0x80000000
 
 export const SIGNING_PATH_PREFIX = "m/424150'/0'/0'"; // BAP in hex
